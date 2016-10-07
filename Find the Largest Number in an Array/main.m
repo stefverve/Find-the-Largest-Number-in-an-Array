20cd7c8856355c365538f70386cbc1ff16fa39e4
stef <stef.verve@gmail.com>
2016-10-07 13:15:13 -0400

    Initial Commit

---
+++ Find the Largest Number in an Array/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Find the Largest Number in an Array
+//
+//  Created by Stefan Verveniotis on 2016-10-07.
+//  Copyright © 2016 Stefan Verveniotis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
